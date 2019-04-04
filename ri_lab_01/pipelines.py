cfda4c044f63bdd085175f5193f82f3cbdbae82f
Marcoozvn <marcos.cesar@ccc.ufcg.edu.br>
2019-04-04 19:04:49 -0300

    Laboratorio 01

--- ri_lab_01/pipelines.py
+++ ri_lab_01/pipelines.py
@@ -5,10 +5,46 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.conf import settings
+from datetime import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
 
 class RiLab01Pipeline(object):
     def process_item(self, item, spider):
+        '''
+        Processamento da string de data, para deixar de acordo com o que a tabela pede
+        '''
+        day, month, year, hour = item['date'].replace('de','').replace('às','').split()
+        month = self.convert_month(month)
+        hh, mm = hour.split(':')
+
+        item['date'] = datetime(int(year), month, int(day), int(hh), int(mm), 00)        
+
         return item
+
+    def convert_month(self, month):
+        if month.lower() == 'janeiro':
+            return 1
+        elif month.lower() == 'fevereiro':
+            return 2
+        elif month.lower() == 'março':
+            return 3
+        elif month.lower() == 'abril':
+            return 4
+        elif month.lower() == 'maio':
+            return 5
+        elif month.lower() == 'junho':
+            return 6
+        elif month.lower() == 'julho':
+            return 7
+        elif month.lower() == 'agosto':
+            return 8
+        elif month.lower() == 'setembro':
+            return 9
+        elif month.lower() == 'outubro':
+            return 10
+        elif month.lower() == 'novembro':
+            return 11
+        elif month.lower() == 'dezembro':
+            return 12
--- ri_lab_01/spiders/brasil_247.py
+++ ri_lab_01/spiders/brasil_247.py
@@ -11,6 +11,8 @@ class Brasil247Spider(scrapy.Spider):
     allowed_domains = ['brasil247.com']
     start_urls = []
 
+    visited_urls = []
+
     def __init__(self, *a, **kw):
         super(Brasil247Spider, self).__init__(*a, **kw)
         with open('seeds/brasil_247.json') as json_file:
@@ -18,14 +20,57 @@ class Brasil247Spider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+        '''
+            Percorre as notícias da main-page, passando para o método encarregado de extrair as informacoes da página da notícia
+        '''
+        for article in response.css('section.section-column'):
+            links = article.css('h3 a::attr(href)').extract()
+            for link in links:
+                yield response.follow(link, self.parse_article)
+
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+
+
+    '''
+    Método encarregado de fazer o parser de cada página de notícia
+    '''
+    def parse_article(self, response):
+        '''
+        Inclui a url na lista de urls visitadas, para evitar duplicacao 
+        '''
+        self.visited_urls.append(response.url)
+
+        related_links = response.css('div.aside-box article a::attr(href)').extract()
+
+        article_link = response.url
+        title = response.css('div.featured-box h1::text').extract_first()
+        subtitle = response.css('div.featured-box p::text').extract_first()
+        date = response.css('div.featured-box p::text').extract()[1]
+        author = response.css('section p strong *::text').extract_first().replace("-", "")
+        text = " ".join(response.css('section p *::text').extract())
+        
+        section_actor_end = text.find('-') + 1
+
+        text = text[section_actor_end:]
+
+        section_url_begin = article_link.find('/247/') + 5
+        section_url_end = article_link.index('/', section_url_begin)
+        section = article_link[section_url_begin:section_url_end]
+
+        item = RiLab01Item(title=title.strip(), sub_title=subtitle.strip(), author=author.strip(), date=date.strip(), url=article_link, text=text.strip(), section=section)
+
+        print(item)
+
+        '''
+        Visita os links em "Matérias relacionadas"
+        '''
+        for link in related_links:
+            if link not in self.visited_urls:
+                print(link)
+                yield response.follow(link, self.parse_article)
+        
+        yield item
